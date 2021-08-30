16d7fb938f795033357b78d2208460cdce705f46
RavenKing <kevinyang.chengkai@gmail.com>
2021-08-30 15:19:24 +0800

    g

--- routes/policys.js
+++ routes/policys.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var uunewid = require('uuid');
 var router = express.Router();
 const {
     ohana
 } = require('ohana-node-orm');
 
+/** */
+function checkData(res,data)
+{
+if(data==null)
+{
+    res.send(500)
+
+}
+
+}
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     const policy = new ohana('SAP_JIANGDU_POLICYS'); // new ohana('table_name');
@@ -12,10 +23,37 @@ router.get('/', function (req, res, next) {
     });
 });
 
+/**update policy  */
+router.put('/',function(req,res,next){
+    checkData(res,req.body.data)
+    const {data} = req.body;
+    console.log(data);
+
+    updatePolicy(data).then((result)=>{
+        console.log(result);
+        if(result ==1 )
+        {
+             res.sendStatus(200)
+        }else{
+            res.send(500)
+        }
+        //return res.send(result)
+    }).catch((err)=>{
+        console.log(err);
+    })
+
+
+});
+
+
 /** insert policys */
 router.post('/', function (req, res, next) {
-    // console.log(req.body)
-    insertData(req.body).then((result)=>{
+     console.log(req.body)
+     const {data} = req.body;
+     console.log(data);
+     //console.log(uunewid.v4())
+     data.POLICY_ID=uunewid.v4();
+    insertData(data).then((result)=>{
         console.log(result);
         if(result ==1 )
         {
@@ -43,6 +81,37 @@ router.post('/', function (req, res, next) {
 async function insertData(body) {
     const policy = new ohana('SAP_JIANGDU_POLICYS'); // new ohana('table_name');
     const result = await policy.insert(body);
+    return result;
+}
+
+/**delete policy  */
+router.delete('/',function(req,res,next){
+    checkData(res,req.body.data);
+    const {data} = req.body;
+    deletePolicy(data).then((result)=>{
+        console.log(result);
+        if(result ==1 )
+        {
+             res.sendStatus(200)
+        }else{
+            res.send(500)
+        }
+        //return res.send(result)
+    }).catch((err)=>{
+        console.log(err);
+    })
+
+})
+
+
+async function updatePolicy(body) {
+    const policy = new ohana('SAP_JIANGDU_POLICYS'); // new ohana('table_name');
+    const result = await policy.update({POLICY_ID:body.POLICY_ID},body);
+    return result;
+}
+async function deletePolicy(body) {
+    const policy = new ohana('SAP_JIANGDU_POLICYS'); // new ohana('table_name');
+   const result = await policy.raw("delete from \"SAP_JIANGDU_POLICYS\" where POLICY_ID = '"+body.POLICY_ID+"'");
     return result;
 }
 
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,54 @@
 var express = require('express');
+var uunewid = require('uuid');
 var router = express.Router();
 const {
   ohana
 } = require('ohana-node-orm');
 
+const tableName="SAP_JIANGDU_USERS";
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  const user = new ohana('SAP_JIANGDU_USERS'); // new ohana('table_name');
+  const user = new ohana(tableName); // new ohana('table_name');
   user.find().then((result) => {
     res.send(result)
   });
 });
+
+/** insert policys */
+router.post('/', function (req, res, next) {
+  const {data} = req.body;
+  //console.log(uunewid.v4())
+  data.USER_ID=uunewid.v4();
+  data.CREATEDAT="2021-08-25 07:59:07.747000000";
+  data.CREATED_AT="2021-08-25 07:59:07.747000000";
+  data.UPDATED_AT="2021-08-25 07:59:07.747000000";
+  data.MODIFIEDAT = "2021-08-25 07:59:07.747000000";
+  data.LOG_DATE = "2021-08-25 07:59:07.747000000";
+  data.VALIDATEDATE = "2021-08-25 07:59:07.747000000";
+  data.HEZHUN_TIME ="2021-08-25 07:59:07.747000000";
+  data.TOKEN="2313122"; 
+  data.COMPANY_CODE="12312312";
+  console.log(data);
+
+ insertData(data).then((result)=>{
+     if(result ==1 )
+     {
+          res.sendStatus(200)
+     }else{
+       console.log(result)
+         res.send(500)
+     }
+     //return res.send(result)
+ }).catch((err)=>{
+     console.log(err);
+ });
+});
+
+
+async function insertData(body) {
+ const user = new ohana(tableName); // new ohana('table_name');
+ const result = await user.insert(body);
+ return result;
+}
 
 module.exports = router;
