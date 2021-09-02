e39f7a7ca6e63689997157c54d7a742a51779f06
Xue <timothy.xue@sap.com>
2021-09-02 16:18:15 +0800

    add jwt verification

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var db = require('./db/db')
+var expressJwt = require('express-jwt')
+var {PRIVITE_KEY,EXPIRESD} = require('./utils/store')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var policysRouter = require('./routes/policys');
 var tagsRouter = require('./routes/tags');
 var talentsRouter = require('./routes/talents');
-
 
 var app = express();
 
@@ -30,6 +31,16 @@ app.use('/api/users', usersRouter);
 app.use('/api/policys', policysRouter);
 app.use('/api/tags', tagsRouter);
 app.use('/api/talents', talentsRouter);
+
+//configure JWT service
+var secret_private_key = "hello jiangdu"
+app.use(expressJwt({
+  credentialsRequired: false,
+  secret: secret_private_key,
+  algorithms: ['HS256']
+}).unless({
+  path: ['/api/users', '/api/policys', '/api/tags', '/api/talents'] //添加不需要验证的路由
+}))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var uunewid = require('uuid');
 var router = express.Router();
+const jwt = require('jsonwebtoken')
+var {PRIVITE_KEY,EXPIRESD} = require("../utils/store")
+
 const {
   ohana
 } = require('ohana-node-orm');
@@ -21,6 +24,77 @@ router.get('/', function (req, res, next) {
   });
 });
 
+// 注册接口
+router.post('/register', async (req, res, next)=>{
+  const user = new ohana(tableName);
+  const{data} = req.body
+  username = data.USER_NAME
+  password = data.PASSWORD
+  try {
+    // 查询当前用户名在不在数据库中(使用async方法后必须使用await方法才有值返回，不然返回promise对象)
+    let user = await user.findOne({USER_NAME: username, PASSWORD: password});
+    // 存在res即是数据库中有数据
+    if (user && user.length != 0) {
+      res.send({
+        code: -1,
+        msg: "用户已注册"
+      })
+    } else {
+      // 对密码进行加密
+    md5_password = md5(username + password)
+      // async 和 await 向数据库插入数据
+    data.USER_ID=uunewid.v4();
+    data.PASSWORD = md5_password;
+    await user.insert(body);
+    res.send({
+        code: 0,
+        msg: "注册成功"
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+});
+
+router.post('/login', async function(req, res, next){
+  const user = new ohana(tableName);
+  username = data.USER_NAME
+  password = data.PASSWORD
+
+  try {
+    let res1 = await user.findOne({USER_NAME: username, PASSWORD: password});
+    let res2 = await user.findOne({COMPANY_CODE: username, PASSWORD: password});
+    let res3 = await user.findOne({PHONE: username, PASSWORD: password});
+  
+    let res = [];
+    res.push(...res1);
+    res.push(...res2);
+    res.push(...res3);
+  
+    let real_user_name = res.USER_NAME
+    md5_password = md5(real_user_name + password)
+  
+    if (res && res.length != 0 && res.PASSWORD == md5_password){
+  
+      let token = jwt.sign(real_user_name, PRIVITE_KEY, {expiresIn:EXPIRESD})
+      res.send({
+        code: 0,
+        user_name: res.USER_NAME,
+        status: 'ok',
+        token: token
+      })
+    } else {
+      res.send({
+          code: -1,
+          msg: "用户名或密码错误！"
+        })
+    }
+  } catch(error) {
+    console.log(error)
+    next(error)
+  }
+})
 
 router.get('/getCompanyInfo', function(req, res, next){
   checkData(res,req.body);
