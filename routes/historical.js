047cc837925ce42e46f7254bbf024949744d5c8d
RavenKing <kevinyang.chengkai@gmail.com>
2021-09-04 11:53:19 +0800

    ///

--- routes/historical.js
+++ routes/historical.js
@@ -13,7 +13,7 @@ function checkData(res, data) {
 }
 
 
-router.get('/latest/policy/companies', async(req, res, next)=>{
+router.post('/latest/policy/companies', async(req, res, next)=>{
     console.log("working")
     //SELECT A1.COMPANY_NAME, A2.COUNT FROM "SAP_JIANGDU_FREQUENTLY_USED_ENTERPRISES" as A2 INNER JOIN "SAP_JIANGDU_USERS" as A1 ON A2.ENTERPRISE_ID_USER_ID = A1.USER_ID
     const histo = new ohana('SAP_JIANGDU_FREQUENTLY_USED_ENTERPRISES');
