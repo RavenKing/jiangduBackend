4b66951edab477b8e98531fffea2b0ecf4a013da
Xue <timothy.xue@sap.com>
2021-09-15 11:25:15 +0800

    enable token verification

--- app.js
+++ app.js
@@ -22,10 +22,12 @@ var finRouter = require('./routes/fin')
 var assetRouter = require('./routes/assets')
 var techRouter=require('./routes/techs')
 
+
 // 使用swagger API 文档
 var swaggerInstall = require('./utils/swagger')
 var app = express();
 swaggerInstall(app)
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -37,6 +39,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//configure JWT service
+app.use(expressJwt({
+  secret: PRIVITE_KEY,
+  algorithms: ['HS256']
+}).unless({
+  path: ['/api/assets', '/api/fins', '/api/recommendLists', '/api/users', '/api/policys', '/api/tags', '/api/talents', '/api/historical','/api/blackLists'] //添加不需要验证的路由
+}))
 
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
@@ -51,14 +61,7 @@ app.use('/api/assets', assetRouter);
 app.use('/api/techs', techRouter);
 app.use('/api/questionnaires', questionnaireRouter);
 
-//configure JWT service
-app.use(expressJwt({
-  credentialsRequired: false,
-  secret: PRIVITE_KEY,
-  algorithms: ['HS256']
-}).unless({
-  path: ['/api/techs','/api/assets', '/api/fins', '/api/recommendLists', '/api/users', '/api/policys', '/api/tags', '/api/talents', '/api/historical','/api/blackLists'] //添加不需要验证的路由
-}))
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
